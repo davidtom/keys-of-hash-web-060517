4f48af08777480153945171808d91bdc384d74a9
davidtom <davidjtomczyk@gmail.com>
2017-05-27 11:12:17 -0400

    completed hash class method

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,9 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*values)
+    return_array = []
+    self.each { |key, value| return_array.push(key) if values.include?(value)}
+    return_array
   end
 end
+
+animals = {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"}
